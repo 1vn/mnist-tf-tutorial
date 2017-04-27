d60a696601ae0a4302717b86c635535ece1509b3
ivan <ivanzhangsolutions@gmail.com>
2017-04-26 23:26:47 -0400

    at least train set has 100% accuracy

--- data/fn.py
+++ data/fn.py
@@ -2,5 +2,7 @@ import tensorflow as tf
 
 
 def input_fn(data_set, batch_size):
-	x, y_ = data_set.next_batch(batch_size)
-	return tf.convert_to_tensor(x, dtype=tf.float64), tf.convert_to_tensor(y_, dtype=tf.float64)
+  x, y_ = data_set.next_batch(batch_size)
+  return tf.convert_to_tensor(
+      x, dtype=tf.float64), tf.convert_to_tensor(
+          y_, dtype=tf.float64)
--- trainer.py
+++ trainer.py
@@ -1,4 +1,3 @@
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -17,34 +16,39 @@ import tensorflow as tf
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
-MODEL_DIR="tmp/training"
+MODEL_DIR = "tmp/training"
+
 
 def accuracy_metric_fn(predictions, labels):
-	correct_prediction = tf.equal(tf.argmax(labels,1), tf.argmax(predictions,1))
-	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-	return accuracy
+  correct_prediction = tf.equal(tf.argmax(labels, 1), tf.argmax(predictions, 1))
+  accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+  return accuracy
 
-def main(_):	
-	mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
-	est = tf.contrib.learn.Estimator(model_fn=model_fn, 
-										model_dir=MODEL_DIR,
-										config=tf.contrib.learn.RunConfig(save_checkpoints_secs=30))
-	exp = tf.contrib.learn.Experiment(estimator=est, 
-										eval_steps=250,
-										min_eval_frequency=500,
-										local_eval_frequency=500,
-										train_input_fn=lambda: input_fn(mnist.train, 100), 
-										eval_input_fn=lambda: input_fn(mnist.test, 100),
-										eval_metrics={"accuracy": accuracy_metric_fn})
+def main(_):
+  mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
-	exp.train_and_evaluate()
-
+  est = tf.contrib.learn.Estimator(
+      model_fn=model_fn,
+      model_dir=MODEL_DIR,
+      config=tf.contrib.learn.RunConfig(save_checkpoints_secs=30))
+  exp = tf.contrib.learn.Experiment(
+      estimator=est,
+      eval_steps=1,
+      min_eval_frequency=500,
+      train_steps=1000,
+      train_input_fn=lambda: input_fn(mnist.train, 100),
+      eval_input_fn=lambda: input_fn(mnist.test, 100),
+      eval_metrics={"accuracy": accuracy_metric_fn})
+  exp.train_and_evaluate()
 
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--data_dir', type=str, default='/tmp/tensorflow/mnist/input_data',
-	                  help='Directory for storing input data')
-	FLAGS, unparsed = parser.parse_known_args()
-	tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+      '--data_dir',
+      type=str,
+      default='/tmp/tensorflow/mnist/input_data',
+      help='Directory for storing input data')
+  FLAGS, unparsed = parser.parse_known_args()
+  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
