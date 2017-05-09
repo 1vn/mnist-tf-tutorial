d094a10e5e6847767cbfe92312b2048ef62089eb
ivan <ivanzhangsolutions@gmail.com>
2017-05-09 14:50:19 -0400

    add dropout layer

--- models/simple_autoencoder.py
+++ models/simple_autoencoder.py
@@ -39,15 +39,22 @@ def autoencoder_fn(features, _, mode):
   tf.summary.image("input", tf.reshape(features, [-1, 28, 28, 1]))
 
   encoder_op = encoder(features)
+
   decoder_op = decoder(encoder_op)
 
-  y_pred = decoder_op
+  dropout_op = tf.nn.dropout(decoder_op, 0.6)
+
+  y_pred = dropout_op
   y_true = features
 
   tf.summary.image("output", tf.reshape(y_pred, [-1, 28, 28, 1]))
 
-  learning_rate = tf.train.exponential_decay(
-      0.1, tf.contrib.framework.get_global_step(), 10000, 0.9, staircase=False)
+  # learning_rate = tf.train.exponential_decay(
+  #     0.001,
+  #     tf.contrib.framework.get_global_step(),
+  #     10000,
+  #     0.9,
+  #     staircase=False)
 
   # cost = tf.reduce_mean(tf.pow(y_true - y_pred, 2)) + tf.nn.l2_loss(
   #     weights['encoder_h1']
@@ -57,7 +64,7 @@ def autoencoder_fn(features, _, mode):
   #             weights['decoder_h2']) + tf.nn.l2_loss(weights['decoder_h3'])
   cost = tf.reduce_mean(tf.pow(y_true - y_pred, 2))
 
-  train_step = tf.train.AdamOptimizer(learning_rate).minimize(
+  train_step = tf.train.AdamOptimizer(0.001).minimize(
       cost, global_step=tf.contrib.framework.get_global_step())
 
   return tf.contrib.learn.ModelFnOps(
