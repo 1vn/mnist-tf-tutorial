45019fde18c46923640ee0630413d02dc8fe2c8b
ivan <ivanzhangsolutions@gmail.com>
2017-05-09 13:52:28 -0400

    0.03

--- models/simple_autoencoder.py
+++ models/simple_autoencoder.py
@@ -2,8 +2,10 @@ import tensorflow as tf
 
 
 def autoencoder_fn(features, _, mode):
-  n_hidden_1 = 256
-  n_hidden_2 = 128
+
+  n_hidden_1 = 50
+  n_hidden_2 = 50
+  n_hidden_3 = 2
   n_input = 784
 
   weights = {
@@ -30,8 +32,8 @@ def autoencoder_fn(features, _, mode):
   def decoder(x):
     layer_1 = tf.nn.sigmoid(
         tf.add(tf.matmul(x, weights['decoder_h1']), biases['decoder_b1']))
-    layer_2 = tf.add(
-        tf.matmul(layer_1, weights['decoder_h2']), biases['decoder_b2'])
+    layer_2 = tf.nn.sigmoid(
+        tf.add(tf.matmul(layer_1, weights['decoder_h2']), biases['decoder_b2']))
     return layer_2
 
   tf.summary.image("input", tf.reshape(features, [-1, 28, 28, 1]))
@@ -47,11 +49,15 @@ def autoencoder_fn(features, _, mode):
   learning_rate = tf.train.exponential_decay(
       0.1, tf.contrib.framework.get_global_step(), 10000, 0.9, staircase=False)
 
-  cost = tf.reduce_mean(tf.pow(y_true - y_pred, 2)) + tf.nn.l2_loss(
-      weights['encoder_h1']) + tf.nn.l2_loss(
-          weights['encoder_h2']) + tf.nn.l2_loss(
-              weights['decoder_h1']) + tf.nn.l2_loss(weights['decoder_h2'])
-  train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(
+  # cost = tf.reduce_mean(tf.pow(y_true - y_pred, 2)) + tf.nn.l2_loss(
+  #     weights['encoder_h1']
+  # ) + tf.nn.l2_loss(weights['encoder_h2']) + tf.nn.l2_loss(
+  #     weights['encoder_h3']) + tf.nn.l2_loss(
+  #         weights['decoder_h1']) + tf.nn.l2_loss(
+  #             weights['decoder_h2']) + tf.nn.l2_loss(weights['decoder_h3'])
+  cost = tf.reduce_mean(tf.pow(y_true - y_pred, 2))
+
+  train_step = tf.train.AdamOptimizer(learning_rate).minimize(
       cost, global_step=tf.contrib.framework.get_global_step())
 
   return tf.contrib.learn.ModelFnOps(
