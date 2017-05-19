175050d8c13e0027b9abe49bdc8d2038b0f636fa
ivan <ivanzhangsolutions@gmail.com>
2017-05-19 13:38:34 -0400

    input fn for lstm

--- data/fn.py
+++ data/fn.py
@@ -1,4 +1,9 @@
+import numpy as np
 import tensorflow as tf
+import data.utils as data_utils
+import random
+
+_buckets = [(5, 10), (10, 15), (20, 25), (40, 50)]
 
 
 def input_fn(data_set, batch_size):
@@ -17,5 +22,25 @@ def autoencoder_input_fn(data_set, batch_size):
   return tf.train.batch([image, image], batch_size=batch_size)
 
 
-def lstm_input_fn(data_set, batch_size):
-  return
+def lstm_input_fn(data_set, batch_size, train_buckets_scale):
+  random_number_01 = np.random.random_sample()
+  bucket_id = min([
+      i for i in xrange(len(train_buckets_scale))
+      if train_buckets_scale[i] > random_number_01
+  ])
+
+  encoder_size, decoder_size = _buckets[bucket_id]
+  encoder_inputs, decoder_inputs = [], []
+  batch_encoder_inputs, batch_decoder_inputs = [], []
+
+  for _ in xrange(batch_size):
+    encoder_input, decoder_input = random.choice(data_set[bucket_id])
+
+    encoder_pad = [data_utils.PAD_ID] * (encoder_size - len(encoder_input))
+    encoder_inputs.append(list(reversed(encoder_input + encoder_pad)))
+
+    decoder_pad_size = decoder_size - len(decoder_input) - 1
+    decoder_inputs.append([data_utils.GO_ID] + decoder_input +
+                          [data_utils.PAD_ID] * decoder_pad_size)
+
+  return tf.train.batch([encoder_inputs, decoder_inputs], batch_size=batch_size)
--- trainer.py
+++ trainer.py
@@ -14,12 +14,38 @@ from models.simple_autoencoder import autoencoder_fn
 from models.simple_lstm import lstm_fn
 
 from data.fn import input_fn, autoencoder_input_fn, lstm_input_fn
+import data.utils as data_utils
 
 import tensorflow as tf
 
 MODEL_DIR = "tmp/training"
 FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string('model', 'simple', 'simple or autoencoder or lstm.')
+
+_buckets = [(5, 10), (10, 15), (20, 25), (40, 50)]
+
+
+#https://github.com/tensorflow/models/blob/master/tutorials/rnn/translate/translate.py
+def read_data(source_path, target_path, max_size=None):
+  data_set = [[] for _ in _buckets]
+  with tf.gfile.GFile(source_path, mode="r") as source_file:
+    with tf.gfile.GFile(target_path, mode="r") as target_file:
+      source, target = source_file.readline(), target_file.readline()
+      counter = 0
+      while source and target and (not max_size or counter < max_size):
+        counter += 1
+        if counter % 100000 == 0:
+          print("  reading data line %d" % counter)
+          sys.stdout.flush()
+        source_ids = [int(x) for x in source.split()]
+        target_ids = [int(x) for x in target.split()]
+        target_ids.append(data_utils.EOS_ID)
+        for bucket_id, (source_size, target_size) in enumerate(_buckets):
+          if len(source_ids) < source_size and len(target_ids) < target_size:
+            data_set[bucket_id].append([source_ids, target_ids])
+            break
+        source, target = source_file.readline(), target_file.readline()
+  return data_set
 
 
 def accuracy_metric_fn(predictions, labels):
@@ -47,14 +73,36 @@ def main(_):
       "accuracy": tf.contrib.learn.MetricSpec(metric_fn=accuracy_metric_fn)
   }
 
+  train_input_fn = None
+  eval_input_fn = None
+
   if FLAGS.model == "autoencoder":
     model_fn = autoencoder_fn
     selected_input_fn = autoencoder_input_fn
     eval_metrics["euclidean distance"] = tf.contrib.learn.MetricSpec(
         metric_fn=euclidean_distance_metric)
   elif FLAGS.model == "lstm":
-    model_fn = lstm_fn
+    from_train, to_train, from_dev, to_dev, _, _ = data_utils.prepare_wmt_data(
+        "LANG_data/",
+        4000,
+        4000,)
     selected_input_fn = lstm_input_fn
+    dev_set = read_data(from_dev, to_dev)
+    train_set = read_data(
+        from_train,
+        to_train,)
+
+    train_bucket_sizes = [len(train_set[b]) for b in xrange(len(_buckets))]
+    train_total_size = float(sum(train_bucket_sizes))
+
+    train_buckets_scale = [
+        sum(train_bucket_sizes[:i + 1]) / train_total_size
+        for i in xrange(len(train_bucket_sizes))
+    ]
+    train_input_fn = lambda: selected_input_fn(train_set, 100, train_buckets_scale)
+    eval_input_fn = lambda: selected_input_fn(dev_set, 100, train_buckets_scale)
+
+    model_fn = lstm_fn
 
   if (FLAGS.model == "simple" or FLAGS.model == "autoencoder"):
     mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
